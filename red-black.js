a054ee166717d05d7263c523effb3829a72dece4
Jackson Gariety <personal@jacksongariety.com>
2013-10-09 23:25:18 -0700

    Adds balanceFromInsertOf() method. Remaining: balanceFromDeletionOf().

--- red-black.js
+++ red-black.js
@@ -1,21 +1,73 @@
 /*
- * Classes
+ * Node
  */
 
 function RedBlackNode(value) {
   this.value = value
   this.left = null
   this.right = null
-  this.height = 1
+  this.color = 'red'
+  this.parent = null
+  this.tree = null
 }
+
+RedBlackNode.prototype.sibling = function () {
+  return this.parent? this === this.parent.left ? this.parent.right : this.parent.left : null
+}
+
+RedBlackNode.prototype.uncle = function () {
+  return this.parent? this.parent.silbing : null
+}
+
+RedBlackNode.prototype.grandparent = function () {
+  return this.parent? this.parent.parent : null
+}
+
+RedBlackNode.prototype.rotateLeft = function () {
+  var right = this.right;
+  this.tree.replaceNode(node, right);
+  
+  // Update pointers
+  this.right = right.left;
+  if (right.left !== null) right.left.parent = this;
+  right.left = this;
+  this.parent = right;
+};
+
+RedBlackNode.prototype.rotateRight = function () {
+  var left = this.left;
+  this.tree.replaceNode(node, left);
+  
+  // Update pointers
+  node.left = left.right;
+  if (left.right !== null) left.right.parent = this;
+  left.right = this;
+  this.parent = left;
+}
+
+/*
+ * Tree
+ */
 
 function RedBlackTree() {
   this.root = null
 }
 
-/*
- * Public Methods
- */
+RedBlackTree.prototype.replaceNode = function (original, replacement) {
+  if (original.parent === null) {
+    original.tree.root = replacement
+  } else {
+    if (original === original.parent.left) {
+      original.parent.left = replacement
+    } else {
+      original.parent.right = replacement
+    }
+  }
+  
+  if (replacement !== null) {
+    replacement.parent = original.parent
+  }
+}
 
 RedBlackTree.prototype.add = function (value) {
   if (this.root === null) {
@@ -23,12 +75,62 @@ RedBlackTree.prototype.add = function (value) {
     return this
   }
   
-  var index = this.root
-  
+  var child = new RedBlackNode(value)
+    , index = this.root
+    
   while (index) {
-    if (value < index.value) { index.left? index = index.left : index.left = new RedBlackNode(value); break }
-    else if (value > index.value) { index.right? index = index.right : index.right = new RedBlackNode(value); break }
-    else break
+    if (value < index.value) {
+      if (index.left) index = index.left
+      else {
+        index.left = child
+        break
+      }
+    } else if (value > index.value) {
+      if (index.right) index = index.right
+      else {
+        index.right = child
+        break 
+      }
+    } else break
+  }
+  
+  child.parent = index
+  child.tree = this
+  
+  return this.balanceFromInsertOf(child)
+}
+
+RedBlackTree.prototype.balanceFromInsertOf = function (child) {
+  var childParent = child.parent
+    , childUncle = child.uncle
+    , childGrandparent = child.grandparent()
+    , childParentColor = childParent.color
+    , childUncleColor = childUncle.color
+  
+  if (!childParent) child.color = 'black'
+  else if (childParentColor == 'black') return this
+  else if (childUncle && childUncleColor === 'red') {
+    childParentColor = 'black'
+    childUncleColor = 'black'
+    childGrandparent.color = 'red'
+    childGrandparent.balance()
+  } else {
+    if (child === child.parent.right && child.parent === child.grandparent.left) {
+      node.parent.rotateLeft()
+      child = node.left
+    } else if (child === child.parent.left && child.parent === child.grandparent.right) {
+      node.parent.rotateRight()
+      child = node.right
+    }
+    
+    child.parent.color = 'black'
+    child.grandparent.color = 'red'
+    
+    if (child === child.parent.left && child.parent === child.grandparent.left) {
+      child.rotateRight(child.grandparent)
+    } else if (child === child.parent.right && child.parent === child.grandparent.right) {
+      child.rotateLeft(child.grandparent)
+    }
   }
   
   return this
@@ -57,21 +159,13 @@ RedBlackTree.prototype.contains = function (value) {
 
 RedBlackTree.prototype.size = function (value) {
   var length = 0
-  
-  this.traverse(function (node) {
-    length++
-  })
-  
+  this.traverse(function (node) { length++ })
   return length
 }
 
 RedBlackTree.prototype.toArray = function () {
   var result = []
-
-  this.traverse(function (node) {
-    result.push(node.value)
-  })
-
+  this.traverse(function (node) { result.push(node.value) })
   return result
 }
 
@@ -79,18 +173,20 @@ RedBlackTree.prototype.toString = function () {
   return this.toArray().toString()
 }
 
-var MyTree = new RedBlackTree()
-
 /* 
  * Tests
  */
+ 
+ var MyTree = new RedBlackTree()
 
-MyTree.add(6)
-MyTree.add(2)
-MyTree.add(10)
+MyTree
+  .add(6)
+  .add(12)
+  .add(3)
+  .add(5)
+  .add(10)
+  .add(32)
 
 MyTree.traverse(function (node) {
-  console.log(node.value)
+  console.log(node.color)
 })
-
-console.log(MyTree.size())
