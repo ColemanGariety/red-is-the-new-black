a8cdf56cd6dd6c0bd01321a76800683c78534d32
Jackson Gariety <personal@jacksongariety.com>
2013-10-10 00:23:50 -0700

    Adds delete method and balanceFromDeletionOf() balancer.

--- red-black.js
+++ red-black.js
@@ -2,13 +2,14 @@
  * Node
  */
 
-function RedBlackNode(value) {
+function RedBlackNode(key, value) {
   this.value = value
   this.left = null
   this.right = null
   this.color = 'red'
   this.parent = null
   this.tree = null
+  this.key = key
 }
 
 RedBlackNode.prototype.sibling = function () {
@@ -24,25 +25,25 @@ RedBlackNode.prototype.grandparent = function () {
 }
 
 RedBlackNode.prototype.rotateLeft = function () {
-  var right = this.right;
-  this.tree.replaceNode(node, right);
+  var right = this.right
+  this.tree.replaceNode(node, right)
   
   // Update pointers
-  this.right = right.left;
-  if (right.left !== null) right.left.parent = this;
-  right.left = this;
-  this.parent = right;
-};
+  this.right = right.left
+  if (right.left !== null) right.left.parent = this
+  right.left = this
+  this.parent = right
+}
 
 RedBlackNode.prototype.rotateRight = function () {
-  var left = this.left;
-  this.tree.replaceNode(node, left);
+  var left = this.left
+  this.tree.replaceNode(node, left)
   
   // Update pointers
-  node.left = left.right;
-  if (left.right !== null) left.right.parent = this;
-  left.right = this;
-  this.parent = left;
+  node.left = left.right
+  if (left.right !== null) left.right.parent = this
+  left.right = this
+  this.parent = left
 }
 
 /*
@@ -69,71 +70,157 @@ RedBlackTree.prototype.replaceNode = function (original, replacement) {
   }
 }
 
-RedBlackTree.prototype.add = function (value) {
+RedBlackTree.prototype.add = function (key, value) {
   if (this.root === null) {
-    this.root = new RedBlackNode(value)
+    this.root = new RedBlackNode(key, value)
     return this
   }
   
-  var child = new RedBlackNode(value)
+  var node = new RedBlackNode(key, value)
     , index = this.root
     
   while (index) {
-    if (value < index.value) {
+    if (key < index.key) {
       if (index.left) index = index.left
       else {
-        index.left = child
+        index.left = node
         break
       }
-    } else if (value > index.value) {
+    } else if (key > index.key) {
       if (index.right) index = index.right
       else {
-        index.right = child
+        index.right = node
         break 
       }
     } else break
   }
   
-  child.parent = index
-  child.tree = this
+  node.parent = index
+  node.tree = this
   
-  return this.balanceFromInsertOf(child)
+  return this.balanceFromInsertOf(node)
 }
 
-RedBlackTree.prototype.balanceFromInsertOf = function (child) {
-  var childParent = child.parent
-    , childUncle = child.uncle
-    , childGrandparent = child.grandparent()
-    , childParentColor = childParent.color
-    , childUncleColor = childUncle.color
+RedBlackTree.prototype.balanceFromInsertOf = function (node) {
+  var nodeParent = node.parent
+    , nodeUncle = node.uncle
+    , nodeGrandparent = node.grandparent()
+    , nodeParentColor = nodeParent.color
+    , nodeUncleColor = nodeUncle.color
   
-  if (!childParent) child.color = 'black'
-  else if (childParentColor == 'black') return this
-  else if (childUncle && childUncleColor === 'red') {
-    childParentColor = 'black'
-    childUncleColor = 'black'
-    childGrandparent.color = 'red'
-    childGrandparent.balance()
+  if (!nodeParent) node.color = 'black'
+  else if (nodeParentColor == 'black') return this
+  else if (nodeUncle && nodeUncleColor === 'red') {
+    nodeParentColor = 'black'
+    nodeUncleColor = 'black'
+    nodeGrandparent.color = 'red'
+    nodeGrandparent.balance()
   } else {
-    if (child === child.parent.right && child.parent === child.grandparent.left) {
+    if (node === node.parent.right && node.parent === node.grandparent.left) {
       node.parent.rotateLeft()
-      child = node.left
-    } else if (child === child.parent.left && child.parent === child.grandparent.right) {
+      node = node.left
+    } else if (node === node.parent.left && node.parent === node.grandparent.right) {
       node.parent.rotateRight()
-      child = node.right
+      node = node.right
     }
     
-    child.parent.color = 'black'
-    child.grandparent.color = 'red'
+    node.parent.color = 'black'
+    node.grandparent.color = 'red'
     
-    if (child === child.parent.left && child.parent === child.grandparent.left) {
-      child.rotateRight(child.grandparent)
-    } else if (child === child.parent.right && child.parent === child.grandparent.right) {
-      child.rotateLeft(child.grandparent)
+    if (node === node.parent.left && node.parent === node.grandparent.left) {
+      node.rotateRight(node.grandparent)
+    } else if (node === node.parent.right && node.parent === node.grandparent.right) {
+      node.rotateLeft(node.grandparent)
     }
   }
   
   return this
+}
+
+RedBlackTree.prototype.get = function (key) {
+  var index = this.root
+  while (index) {
+    if (key === index.key) return index.value
+    else if (key < index.key) index = index.left
+    else if (key > index.key) index = index.right
+  }
+  
+  return false
+}
+
+RedBlackTree.prototype.delete = function (key) {
+  var node = this.root
+  while (node) {
+    if (key === node.key) break
+    else if (key < node.key) node = node.left
+    else if (key > node.key) node = node.right
+  }
+  
+  if (node.left !== null && node.right !== null) {
+    var pred = node.left;
+    while (pred.right !== null) pred = pred.right;
+    
+    node.key = pred.key;
+    node.value = pred.value;
+    node = pred;
+  }
+  
+  var child = (node.right === null) ? node.left : node.right
+  if (node.color === 'black') {
+    node.color = child.color
+    this.balanceFromDeletionOf(node)
+  }
+  
+  this.replaceNode(node, child)
+  this.root.color = 'black'
+  
+  return this
+}
+
+RedBlackTree.prototype.balanceFromDeletionOf = function (node) {
+  if (node.parent !== null) {
+    var sibling = node.sibling()
+    
+    if (sibling && sibling.color === 'red') {
+      node.parent.color = 'red'
+      sibling.color = 'black'
+      if (node === node.parent.left) node.parent.rotateLeft()
+      else node.parent.rotateRight()
+    }
+    
+    if (node.parent && node.parent.color === 'black' && sibling && sibling.color === 'black' && sibling.left.color === 'black' && sibling.right.color === 'black') {
+      sibling.color = 'red'
+      this.balanceFromDeletionOf(node.parent)
+    } else {
+      if (node.parent && node.parent.color === 'red' && sibling && sibling.color === 'black' && sibling.left.color === 'black' && sibling.right.color === 'black') {
+        sibling.color = 'red'
+        node.parent.color = 'black'
+      } else {
+        if (node.parent && node === node.parent.left && sibling && sibling.color === 'black' && sibling.left.color === 'red' && sibling.right.color === 'black') {
+          sibling.color = 'red'
+          sibling.left.color = 'black'
+          sibling.rotateRight()
+        } else if (node.parent && node === node.parent.right && sibling && sibling.color === 'black' && sibling.right.color === 'red' && sibling.left.color === 'black') {
+          sibling.color = 'red'
+          sibling.right.color = 'black'
+          sibling.rotateLeft()
+        }
+        
+        if (sibling) {
+          sibling.color = node.parent.color
+          node.parent.color = 'black'
+          
+          if (node === node.parent.left) {
+            sibling.right.color = 'black'
+            node.parent.rotateLeft()
+          } else {
+            sibling.left.color = 'black'
+            node.parent.rotateRight()
+          }
+        }
+      }
+    }
+  }
 }
 
 RedBlackTree.prototype.traverse = function (process) {
@@ -177,16 +264,20 @@ RedBlackTree.prototype.toString = function () {
  * Tests
  */
  
- var MyTree = new RedBlackTree()
+var MyTree = new RedBlackTree()
 
 MyTree
-  .add(6)
-  .add(12)
-  .add(3)
-  .add(5)
-  .add(10)
-  .add(32)
+  .add('z', 6)
+  .add('b', 12)
+  .add('c', 3)
+  .add('d', 5)
+  .add('e', 10)
+  .add('f', 32)
+  .delete('d')
+  .add('g', 234)
 
 MyTree.traverse(function (node) {
   console.log(node.color)
 })
+
+console.log(MyTree.toString())
